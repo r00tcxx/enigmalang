d00b7a50dc2767de5770e2f90f7c00d26f6efdf1
r00tcxx <r00tcxx@gmail.com>
2023-06-19 19:10:30 +0800

    feat(ts): add function template class

--- src/core/main.cpp
+++ src/core/main.cpp
@@ -3,17 +3,8 @@
 #include <functional>
 #include <vector>
 
-/*
-	build
-		-o output file
-		-O optimize
-		file
-	run
-		-O optimize
-		file
-	version
-	help
-*/
+#include "tk/function.h"
+#include "tk/result.h"
 
 bool command_build() {
 	return false;
@@ -31,20 +22,34 @@ struct Command {
 
 };
 
-int main(int argc, char* argv[]) {
-	std::unordered_map<std::string, std::function<bool(const Command&)>> commands {
-		{ "build",		tk::to_function(command_build) },
-		{ "run",		tk::to_function(command_run) },
-		{ "version",	tk::to_function(command_version) }
-	};
 
-	auto[error, cmd] = ParseCommand(argc, argv);
-	if (error)
-		log::info("enigmalang: {}", error.string());
-	if (auto match_command = cmd.empty() ? commands.end() : commands.find(cmd.name()); match_command != commands.end())
-		return !match_command->second(cmd);
-	else
-		print_useage();
-	return 1;
+tk::result_t<std::string, int> foo() {
+
+	return  { tk::error(1, "123") };
+}
+
+int main(int argc, char* argv[]) {
+	//std::unordered_map<std::string, std::function<bool(const Command&)>> commands {
+	//	{ "build",		tk::to_function(command_build) },
+	//	{ "run",		tk::to_function(command_run) },
+	//	{ "version",	tk::to_function(command_version) }
+	//};
+
+	auto [error, ret1, ret2] = foo();
+	if (error) {
+		error.code();
+		error.string();
+	}
+
+	return 0;
+
+	//auto[error, cmd] = ParseCommand(argc, argv);
+	//if (error)
+	//	log::info("enigmalang: {}", error.string());
+	//if (auto match_command = cmd.empty() ? commands.end() : commands.find(cmd.name()); match_command != commands.end())
+	//	return !match_command->second(cmd);
+	//else
+	//	print_useage();
+	//return 1;
 }
 
